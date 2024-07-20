255b686bf974956d314417c6e3cbb069b049afb4
StarySky1118 <107917438+StarySky1118@users.noreply.github.com>
2024-07-20 08:48:41 +0000

    Deploying to gh-pages from @ StarySky1118/yi-doc@b9060754f17ff8b6b60592f5f743dd8bc8fb3da0 🚀

---
+++ assets/go_keywords-C3MbmCWG.js
@@ -0,0 +1,1 @@
+const o="/yi-doc/images/dev/go/go_basic/go_keywords.png";export{o as _};
---
+++ assets/hello_go-DdQ0IQeB.js
@@ -0,0 +1,1 @@
+const o="/yi-doc/images/dev/go/go_basic/go_installer.png",s="/yi-doc/images/dev/go/go_basic/hello_go.png";export{o as _,s as a};
